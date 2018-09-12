17de1e9dc888811d0e1703dc210c5deb2861b9e1
Andre601 <11576465+Andre601@users.noreply.github.com>
2018-09-12 12:43:50 +0200

    No idea if that fixes the double-packages...

---
+++ src/main/java/com/andre601/helpgui/util/config/ConfigPaths.java
@@ -0,0 +1,36 @@
+package com.andre601.helpgui.util.config;
+
+public final class ConfigPaths {
+
+    public static String DP_MODE          = "Main.DisabledPlayers.Mode";
+    public static String DISABLED_PLAYERS = "Main.DisabledPlayers.Players";
+    public static String DW_MODE          = "Main.DisabledWorlds.Mode";
+    public static String DISABLED_WORLDS  = "Main.DisabledWorlds.Worlds";
+
+
+    public static String MSG_INV_TITLE         = "Messages.HelpInv.Title";
+    public static String MSG_INV_INFO          = "Messages.HelpInv.Info";
+    public static String MSG_INV_INFO_DESC     = "Messages.HelpInv.InfoDesc";
+    public static String MSG_NEXT_PAGE         = "Messages.HelpInv.NextPage";
+    public static String MSG_PREV_PAGE         = "Messages.HelpInv.PrevPage";
+    public static String MSG_SEARCH_INV_TITLE  = "Messages.SearchInv.Title";
+    public static String MSG_PLAYERS_FOUND     = "Messages.SearchInv.PlayersFound";
+    public static String MSG_PLUGIN_LOADED     = "Messages.Startup.PluginLoaded";
+    public static String MSG_VAULT_FOUND       = "Messages.Startup.VaultFound";
+    public static String MSG_VAULT_NOT_FOUND   = "Messages.Startup.VaultNotFound";
+    public static String MSG_NO_PLAYER         = "Messages.Console.NoPlayer";
+    public static String MSG_HELP_SEND         = "Messages.HelpRequest.Sender";
+    public static String MSG_HELP_RECEIVED     = "Messages.HelpRequest.Recipient";
+    public static String ERR_CMD_DISABLED      = "Messages.Errors.CommandDisabled";
+    public static String ERR_NOT_ONLINE        = "Messages.Errors.NotOnline";
+    public static String ERR_DISABLED_WORLD    = "Messages.Errors.DisabledWorld";
+    public static String ERR_NO_PLAYERS_FOUND  = "Messages.Errors.NoPlayersFound";
+    public static String ERR_NO_GROUP          = "Messages.Errors.NoGroup";
+    public static String ERR_VAULT_NOT_ENABLED = "Messages.Errors.VaultNotEnabled";
+    public static String MSG_FEW_ARGS          = "Messages.Commands.FewArgs";
+    public static String MSG_UNKNOWN_ARGS      = "Messages.Commands.UnknownArg";
+    public static String MSG_NO_PERM           = "Messages.Commands.NoPerm";
+    public static String MSG_CMD_DESCRIPTION   = "Messages.Commands.CommandDescription";
+
+
+}
